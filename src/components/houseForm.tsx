e26c347f024ac1c40d6149840cc1d9a6d3bcf43b
Manan Gupta <guptamanan1991@gmail.com>
2021-05-16 01:53:50 +0530

    Lesson 15 finsihed: Added functionality to create entry for new house on the database using Prisma and GraphQL

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -2,14 +2,14 @@ import { useState, useEffect, ChangeEvent } from "react";
 import react from "react";
 import { useForm } from "react-hook-form";
 import { useMutation, gql } from "@apollo/client";
-// import { useRouter } from "next/router";
+import { useRouter } from "next/router";
 import Link from "next/link";
 // import { Image } from "cloudinary-react";
 import { SearchBox } from "./searchBox";
-// import {
-//   CreateHouseMutation,
-//   CreateHouseMutationVariables,
-// } from "src/generated/CreateHouseMutation";
+import {
+  CreateHouseMutation,
+  CreateHouseMutationVariables,
+} from "src/generated/CreateHouseMutation";
 // import {
 //   UpdateHouseMutation,
 //   UpdateHouseMutationVariables,
@@ -21,6 +21,14 @@ const SIGNATURE_MUTATION = gql`
     createImageSignature {
       signature
       timestamp
+    }
+  }
+`;
+
+const CREATE_HOUSE_MUTATION = gql`
+  mutation CreateHouseMutation($input: HouseInput!) {
+    createHouse(input: $input) {
+      id
     }
   }
 `;
@@ -59,6 +67,7 @@ interface IFormData {
 interface IProps {}
 
 export default function HouseForm({}: IProps) {
+  const router = useRouter();
   const [submitting, setSubmitting] = useState(false);
   const [previewImage, setPreviewImage] = useState<string>();
   const { register, handleSubmit, setValue, errors, watch } = useForm<
@@ -68,6 +77,10 @@ export default function HouseForm({}: IProps) {
   const [createSignature] = useMutation<CreateSignatureMutation>(
     SIGNATURE_MUTATION
   );
+  const [createHouse] = useMutation<
+    CreateHouseMutation,
+    CreateHouseMutationVariables
+  >(CREATE_HOUSE_MUTATION);
 
   useEffect(() => {
     register({ name: "address" }, { required: "Please enter your address" });
@@ -80,7 +93,23 @@ export default function HouseForm({}: IProps) {
     if (signatureData) {
       const { signature, timestamp } = signatureData.createImageSignature;
       const imageData = await uploadImage(data.image[0], signature, timestamp);
-      console.log(imageData);
+
+      const { data: houseData } = await createHouse({
+        variables: {
+          input: {
+            address: data.address,
+            image: imageData.secure_url,
+            coordinates: {
+              latitude: data.latitude,
+              longitude: data.longitude,
+            },
+            bedrooms: parseInt(data.bedrooms, 10),
+          },
+        },
+      });
+      if (houseData?.createHouse) {
+        router.push(`/houses/${houseData.createHouse.id}`);
+      }
     }
   };
 
@@ -108,7 +137,6 @@ export default function HouseForm({}: IProps) {
         {errors.address && (
           <p className="text-red-400">{errors.address.message}</p>
         )}
-        <h2>{address}</h2> {/*Could be removed later*/}
       </div>
 
       {address && (
