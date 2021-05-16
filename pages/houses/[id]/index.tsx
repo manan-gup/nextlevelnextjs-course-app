bb4362579fc1322857b43c2dc5731066bf39760c
Manan Gupta <guptamanan1991@gmail.com>
2021-05-16 19:27:49 +0530

    Lesson 17 finished: Added map with marker to single house page

--- pages/houses/[id]/index.tsx
+++ pages/houses/[id]/index.tsx
@@ -3,7 +3,7 @@ import { Image } from "cloudinary-react";
 import { useQuery, gql } from "@apollo/client";
 import Layout from "src/components/layout";
 // import HouseNav from "src/components/houseNav";
-// import SingleMap from "src/components/singleMap";
+import SingleMap from "src/components/singleMap";
 import {
   ShowHouseQuery,
   ShowHouseQueryVariables,
@@ -68,7 +68,9 @@ function HouseData({ id }: { id: string }) {
             />
             <p>{house.bedrooms} 🛏 house</p>
           </div>
-          <div className="sm:w-full md:w-1/2">SingleMap</div>
+          <div className="sm:w-full md:w-1/2">
+            <SingleMap house={house} />
+          </div>
         </div>
       }
     />
